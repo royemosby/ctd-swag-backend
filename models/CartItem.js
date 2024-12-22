cd8c584a3d364b3b751be856fb8ed770a7f69b85
Roy Mosby <ego.madking@gmail.com>
2024-12-21 19:08:53 -0500

    add findByUserId, upsertItem

--- models/CartItem.js
+++ models/CartItem.js
@@ -3,6 +3,16 @@
 const { Model } = require('objection');
 
 class CartItem extends Model {
+  static productFields = [
+    'baseName',
+    'variantName',
+    'price',
+    'baseDescription',
+    'variantDescription',
+    'image',
+    'inStock',
+  ];
+
   static get tableName() {
     return 'cart_items';
   }
@@ -41,6 +51,42 @@ class CartItem extends Model {
         },
       },
     };
+  }
+  static async findByUserId(userId) {
+    return (
+      CartItem.query()
+        .where({ userId })
+        .joinRelated('product')
+        .select(
+          'cart_items.*',
+          this.productFields.map((i) => `product.${i}`)
+        ) || []
+    );
+  }
+
+  static async upsertItem({ userId, productId }) {
+    let [cartItem] = await CartItem.query().where({ userId, productId });
+    if (cartItem) {
+      cartItem = await CartItem.query().patchAndFetchById(cartItem.id, {
+        quantity: cartItem.quantity + 1,
+      });
+    } else {
+      cartItem = await CartItem.query()
+        .insertAndFetch({
+          userId,
+          productId,
+          quantity: 1,
+        })
+        .withGraphFetched('product');
+    }
+
+    return CartItem.query()
+      .where({ userId, productId })
+      .joinRelated('product')
+      .select(
+        'cart_items.*',
+        this.productFields.map((i) => `product.${i}`)
+      );
   }
 }
 
