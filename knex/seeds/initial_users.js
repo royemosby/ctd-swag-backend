c51b61d4b2b4666e6119db276acca56da1bed6f0
Roy Mosby <ego.madking@gmail.com>
2024-12-23 16:31:14 -0500

    give users hashed passwords

--- knex/seeds/initial_users.js
+++ knex/seeds/initial_users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -5,7 +6,11 @@
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('users').del();
-  await knex('users').insert([
+  await knex('users').insert(await seedUsers());
+};
+
+const seedUsers = async () => {
+  const users = [
     {
       firstName: 'Joe',
       lastName: 'Simpson',
@@ -18,5 +23,17 @@ exports.seed = async function (knex) {
       email: 's.w@test.com',
       password: 'password2',
     },
-  ]);
+    {
+      firstName: 'First',
+      lastName: 'Last',
+      email: 'dev@test.com',
+      password: 'password123',
+    },
+  ];
+
+  for (const user of users) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+
+  return users;
 };
